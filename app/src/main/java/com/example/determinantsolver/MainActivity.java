4552241d6a502c1df304af8d80fb1989903140d5
Avirup Banerjee <julimukherjee23@gmail.com>
2022-02-21 16:34:04 +0530

    Added a more elaborate solution

--- app/src/main/java/com/example/determinantsolver/MainActivity.java
+++ app/src/main/java/com/example/determinantsolver/MainActivity.java
@@ -44,90 +44,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void solve(@NotNull View view){
         try {
-            if (Objects.equals(field_a1.getText().toString(), "") || Objects.equals(field_a2.getText().toString(), "") || Objects.equals(field_a3.getText().toString(), "") || Objects.equals(field_b1.getText().toString(), "") || Objects.equals(field_b2.getText().toString(), "") || Objects.equals(field_b3.getText().toString(), "") || Objects.equals(field_c1.getText().toString(), "") || Objects.equals(field_c2.getText().toString(), "") || Objects.equals(field_c3.getText().toString(), ""))
-                return;
-            double field_a1_value = Double.parseDouble(field_a1.getText().toString()),
-                    field_a2_value = Double.parseDouble(field_a2.getText().toString()),
-                    field_a3_value = Double.parseDouble(field_a3.getText().toString()),
-                    field_b1_value = Double.parseDouble(field_b1.getText().toString()),
-                    field_b2_value = Double.parseDouble(field_b2.getText().toString()),
-                    field_b3_value = Double.parseDouble(field_b3.getText().toString()),
-                    field_c1_value = Double.parseDouble(field_c1.getText().toString()),
-                    field_c2_value = Double.parseDouble(field_c2.getText().toString()),
-                    field_c3_value = Double.parseDouble(field_c3.getText().toString());
-
-            double result = field_a1_value * (field_b2_value * field_c3_value - field_b3_value * field_c2_value)
-                    - field_a2_value * (field_b1_value * field_c3_value - field_b3_value * field_c1_value)
-                    + field_a3_value * (field_b1_value * field_c2_value - field_b2_value * field_c1_value);
-            result = Math.round(result*100)/100.0;
-
-            String result_string = String.valueOf(Double.valueOf(result));
-
-            field_answer.setText(result_string);
-
-            String solution = """
-                    $result = [($field_a1_value)×{($field_b2_value)×($field_c3_value) - ($field_b3_value)×($field_c2_value)}
-                              - ($field_a2_value)×{($field_b1_value)×($field_c3_value) - ($field_b3_value)×($field_c1_value)}
-                              + ($field_a3_value){($field_b1_value)×($field_c2_value) - ($field_b2_value)×($field_c1_value)}]"""
-                    .replace("$result", result_string)
-                    .replace("$field_a1_value", String.valueOf(field_a1_value))
-                    .replace("$field_a2_value", String.valueOf(field_a2_value))
-                    .replace("$field_a3_value", String.valueOf(field_a3_value))
-                    .replace("$field_b1_value", String.valueOf(field_b1_value))
-                    .replace("$field_b2_value", String.valueOf(field_b2_value))
-                    .replace("$field_b3_value", String.valueOf(field_b3_value))
-                    .replace("$field_c1_value", String.valueOf(field_c1_value))
-                    .replace("$field_c2_value", String.valueOf(field_c2_value))
-                    .replace("$field_c3_value", String.valueOf(field_c3_value));
-            text_solution.setText(solution);
-            label_exception.setVisibility(View.INVISIBLE);
+            solve_simple();
         }catch(Exception e) {
             try {
-                String toBeSolved = "[($field_a1_value)×{($field_b2_value)×($field_c3_value) - ($field_b3_value)×($field_c2_value)}-($field_a2_value)×{($field_b1_value)×($field_c3_value) - ($field_b3_value)×($field_c1_value)}+($field_a3_value){($field_b1_value)×($field_c2_value) - ($field_b2_value)×($field_c1_value)}]"
-                        .replace("{", "(")
-                        .replace("[", "(")
-                        .replace("}", ")")
-                        .replace("]", ")")
-                        .replace("×", "*")
-                        .replace("$field_a1_value", field_a1.getText().toString())
-                        .replace("$field_a2_value", field_a2.getText().toString())
-                        .replace("$field_a3_value", field_a3.getText().toString())
-                        .replace("$field_b1_value", field_b1.getText().toString())
-                        .replace("$field_b2_value", field_b2.getText().toString())
-                        .replace("$field_b3_value", field_b3.getText().toString())
-                        .replace("$field_c1_value", field_c1.getText().toString())
-                        .replace("$field_c2_value", field_c2.getText().toString())
-                        .replace("$field_c3_value", field_c3.getText().toString());
-                Expression expression = new ExpressionBuilder(toBeSolved).build();
-                double result = expression.evaluate();
-                field_answer.setText(String.valueOf(result));
-
-                String solution = """
-                        $result = [($field_a1_value)×{($field_b2_value)×($field_c3_value) - ($field_b3_value)×($field_c2_value)}
-                                  - ($field_a2_value)×{($field_b1_value)×($field_c3_value) - ($field_b3_value)×($field_c1_value)}
-                                  + ($field_a3_value){($field_b1_value)×($field_c2_value) - ($field_b2_value)×($field_c1_value)}]"""
-                        .replace("$result", String.valueOf(result))
-                        .replace("$field_a1_value", field_a1.getText().toString())
-                        .replace("$field_a2_value", field_a2.getText().toString())
-                        .replace("$field_a3_value", field_a3.getText().toString())
-                        .replace("$field_b1_value", field_b1.getText().toString())
-                        .replace("$field_b2_value", field_b2.getText().toString())
-                        .replace("$field_b3_value", field_b3.getText().toString())
-                        .replace("$field_c1_value", field_c1.getText().toString())
-                        .replace("$field_c2_value", field_c2.getText().toString())
-                        .replace("$field_c3_value", field_c3.getText().toString());
-
-                text_solution.setText(solution);
-                label_exception.setVisibility(View.INVISIBLE);
-
+                solve_expression();
             } catch (IllegalArgumentException illegalArgumentException) {
-                illegalArgumentException.printStackTrace();
                 text_solution.setText("");
                 field_answer.setText("");
                 label_exception.setText(R.string.invalid_expression_text);
                 label_exception.setVisibility(View.VISIBLE);
             } catch (Exception exception) {
-                exception.printStackTrace();
                 text_solution.setText("");
                 field_answer.setText("");
                 label_exception.setText(R.string.invalid_input_text);
@@ -166,5 +92,140 @@ public class MainActivity extends AppCompatActivity {
         submit_button = findViewById(R.id.submit_button);
         reset_button = findViewById(R.id.reset_button);
         label_exception = findViewById(R.id.text_exception);
+    }
+
+    private void solve_simple() throws NumberFormatException {
+        if (Objects.equals(field_a1.getText().toString(), "") || Objects.equals(field_a2.getText().toString(), "") || Objects.equals(field_a3.getText().toString(), "") || Objects.equals(field_b1.getText().toString(), "") || Objects.equals(field_b2.getText().toString(), "") || Objects.equals(field_b3.getText().toString(), "") || Objects.equals(field_c1.getText().toString(), "") || Objects.equals(field_c2.getText().toString(), "") || Objects.equals(field_c3.getText().toString(), ""))
+            return;
+        double field_a1_value = Double.parseDouble(field_a1.getText().toString()),
+                field_a2_value = Double.parseDouble(field_a2.getText().toString()),
+                field_a3_value = Double.parseDouble(field_a3.getText().toString()),
+                field_b1_value = Double.parseDouble(field_b1.getText().toString()),
+                field_b2_value = Double.parseDouble(field_b2.getText().toString()),
+                field_b3_value = Double.parseDouble(field_b3.getText().toString()),
+                field_c1_value = Double.parseDouble(field_c1.getText().toString()),
+                field_c2_value = Double.parseDouble(field_c2.getText().toString()),
+                field_c3_value = Double.parseDouble(field_c3.getText().toString());
+
+        double result = field_a1_value * (field_b2_value * field_c3_value - field_b3_value * field_c2_value)
+                - field_a2_value * (field_b1_value * field_c3_value - field_b3_value * field_c1_value)
+                + field_a3_value * (field_b1_value * field_c2_value - field_b2_value * field_c1_value);
+        result = Math.round(result * 100) / 100.0;
+
+        String result_string = String.valueOf(Double.valueOf(result));
+
+        String solution = """
+                $result = [($field_a1_value)×{($field_b2_value)×($field_c3_value) - ($field_b3_value)×($field_c2_value)}
+                          - ($field_a2_value)×{($field_b1_value)×($field_c3_value) - ($field_b3_value)×($field_c1_value)}
+                          + ($field_a3_value)×{($field_b1_value)×($field_c2_value) - ($field_b2_value)×($field_c1_value)}]"""
+                .replace("$result", result_string)
+                .replace("$field_a1_value", String.valueOf(field_a1_value))
+                .replace("$field_a2_value", String.valueOf(field_a2_value))
+                .replace("$field_a3_value", String.valueOf(field_a3_value))
+                .replace("$field_b1_value", String.valueOf(field_b1_value))
+                .replace("$field_b2_value", String.valueOf(field_b2_value))
+                .replace("$field_b3_value", String.valueOf(field_b3_value))
+                .replace("$field_c1_value", String.valueOf(field_c1_value))
+                .replace("$field_c2_value", String.valueOf(field_c2_value))
+                .replace("$field_c3_value", String.valueOf(field_c3_value));
+
+        field_answer.setText(result_string);
+        text_solution.setText(solution);
+        label_exception.setVisibility(View.INVISIBLE);
+    }
+
+    private void solve_expression() {
+        String toBeSolved = "[($field_a1_value)×{($field_b2_value)×($field_c3_value) - ($field_b3_value)×($field_c2_value)}-($field_a2_value)×{($field_b1_value)×($field_c3_value) - ($field_b3_value)×($field_c1_value)}+($field_a3_value){($field_b1_value)×($field_c2_value) - ($field_b2_value)×($field_c1_value)}]"
+                .replace("{", "(")
+                .replace("[", "(")
+                .replace("}", ")")
+                .replace("]", ")")
+                .replace("×", "*")
+                .replace("$field_a1_value", field_a1.getText().toString())
+                .replace("$field_a2_value", field_a2.getText().toString())
+                .replace("$field_a3_value", field_a3.getText().toString())
+                .replace("$field_b1_value", field_b1.getText().toString())
+                .replace("$field_b2_value", field_b2.getText().toString())
+                .replace("$field_b3_value", field_b3.getText().toString())
+                .replace("$field_c1_value", field_c1.getText().toString())
+                .replace("$field_c2_value", field_c2.getText().toString())
+                .replace("$field_c3_value", field_c3.getText().toString());
+        Expression expression = new ExpressionBuilder(toBeSolved).build();
+        double result = expression.evaluate();
+        field_answer.setText(String.valueOf(result));
+
+        String field_a1_abs_value = String.valueOf(new ExpressionBuilder(field_a1.getText().toString()).build().evaluate()),
+                field_a2_abs_value = String.valueOf(new ExpressionBuilder(field_a2.getText().toString()).build().evaluate()),
+                field_a3_abs_value = String.valueOf(new ExpressionBuilder(field_a3.getText().toString()).build().evaluate()),
+                field_b1_abs_value = String.valueOf(new ExpressionBuilder(field_b1.getText().toString()).build().evaluate()),
+                field_b2_abs_value = String.valueOf(new ExpressionBuilder(field_b2.getText().toString()).build().evaluate()),
+                field_b3_abs_value = String.valueOf(new ExpressionBuilder(field_b3.getText().toString()).build().evaluate()),
+                field_c1_abs_value = String.valueOf(new ExpressionBuilder(field_c1.getText().toString()).build().evaluate()),
+                field_c2_abs_value = String.valueOf(new ExpressionBuilder(field_c2.getText().toString()).build().evaluate()),
+                field_c3_abs_value = String.valueOf(new ExpressionBuilder(field_c3.getText().toString()).build().evaluate());
+
+        String field_b2_c3_value = String.valueOf(Double.parseDouble(field_b2_abs_value) * Double.parseDouble(field_c3_abs_value)),
+                field_b3_c2_value = String.valueOf(Double.parseDouble(field_b3_abs_value) * Double.parseDouble(field_c2_abs_value)),
+                field_b1_c3_value = String.valueOf(Double.parseDouble(field_b1_abs_value) * Double.parseDouble(field_c3_abs_value)),
+                field_b3_c1_value = String.valueOf(Double.parseDouble(field_b3_abs_value) * Double.parseDouble(field_c1_abs_value)),
+                field_b1_c2_value = String.valueOf(Double.parseDouble(field_b1_abs_value) * Double.parseDouble(field_c2_abs_value)),
+                field_b2_c1_value = String.valueOf(Double.parseDouble(field_b2_abs_value) * Double.parseDouble(field_c1_abs_value));
+
+
+        String solution = """
+                [($field_a1_value)×{($field_b2_value)×($field_c3_value) - ($field_b3_value)×($field_c2_value)}
+                 - ($field_a2_value)×{($field_b1_value)×($field_c3_value) - ($field_b3_value)×($field_c1_value)}
+                 + ($field_a3_value)×{($field_b1_value)×($field_c2_value) - ($field_b2_value)×($field_c1_value)}]
+                 
+                 = [$field_a1_abs_value×{$field_b2_abs_value×$field_c3_abs_value - $field_b3_abs_value×$field_c2_abs_value}
+                 - $field_a2_abs_value×{$field_b1_abs_value×$field_c3_abs_value - $field_b3_abs_value×$field_c1_abs_value}
+                 + $field_a3_abs_value×{$field_b1_abs_value×$field_c2_abs_value - $field_b2_abs_value×$field_c1_abs_value}]
+                 
+                 = [$field_a1_abs_value×{$field_b2*c3_value - $field_b3*c2_value}
+                 - $field_a2_abs_value×{$field_b1*c3_value - $field_b3*c1_value}
+                 + $field_a3_abs_value×{$field_b1*c2_value - $field_b2*c1_value}]
+                 
+                 = $field_a1*b2*c3_value - $field_a1*b3*c2_value - $field_a2*b1*c3_value + $field_a2*b3*c1_value + $field_a3*b1*c2_value - $field_a3*b2*c1_value
+                 
+                 = $result"""
+
+                .replace("$field_a1_value", field_a1.getText().toString())
+                .replace("$field_a2_value", field_a2.getText().toString())
+                .replace("$field_a3_value", field_a3.getText().toString())
+                .replace("$field_b1_value", field_b1.getText().toString())
+                .replace("$field_b2_value", field_b2.getText().toString())
+                .replace("$field_b3_value", field_b3.getText().toString())
+                .replace("$field_c1_value", field_c1.getText().toString())
+                .replace("$field_c2_value", field_c2.getText().toString())
+                .replace("$field_c3_value", field_c3.getText().toString())
+
+                .replace("$field_a1_abs_value", field_a1_abs_value)
+                .replace("$field_a2_abs_value", field_a2_abs_value)
+                .replace("$field_a3_abs_value", field_a3_abs_value)
+                .replace("$field_b1_abs_value", field_b1_abs_value)
+                .replace("$field_b2_abs_value", field_b2_abs_value)
+                .replace("$field_b3_abs_value", field_b3_abs_value)
+                .replace("$field_c1_abs_value", field_c1_abs_value)
+                .replace("$field_c2_abs_value", field_c2_abs_value)
+                .replace("$field_c3_abs_value", field_c3_abs_value)
+
+                .replace("$field_b2*c3_value", field_b2_c3_value)
+                .replace("$field_b3*c2_value", field_b3_c2_value)
+                .replace("$field_b1*c3_value", field_b1_c3_value)
+                .replace("$field_b3*c1_value", field_b3_c1_value)
+                .replace("$field_b1*c2_value", field_b1_c2_value)
+                .replace("$field_b2*c1_value", field_b2_c1_value)
+
+                .replace("$field_a1*b2*c3_value", String.valueOf(Double.parseDouble(field_a1_abs_value) * Double.parseDouble(field_b2_c3_value)))
+                .replace("$field_a1*b3*c2_value", String.valueOf(Double.parseDouble(field_a1_abs_value) * Double.parseDouble(field_b3_c2_value)))
+                .replace("$field_a2*b1*c3_value", String.valueOf(Double.parseDouble(field_a2_abs_value) * Double.parseDouble(field_b1_c3_value)))
+                .replace("$field_a2*b3*c1_value", String.valueOf(Double.parseDouble(field_a2_abs_value) * Double.parseDouble(field_b3_c1_value)))
+                .replace("$field_a3*b1*c2_value", String.valueOf(Double.parseDouble(field_a3_abs_value) * Double.parseDouble(field_b1_c2_value)))
+                .replace("$field_a3*b2*c1_value", String.valueOf(Double.parseDouble(field_a3_abs_value) * Double.parseDouble(field_b2_c1_value)))
+
+                .replace("$result", String.valueOf(result));
+
+        text_solution.setText(solution);
+        label_exception.setVisibility(View.INVISIBLE);
     }
 }
